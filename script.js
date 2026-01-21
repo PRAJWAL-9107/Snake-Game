0323c773dbb4dbff4c06d3193c91af46c7450835
PRAJWAL-9107 <pjadhav91079107@gmail.com>
2026-01-21 18:13:02 +0530

    reduced code

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const blockSize = 30;
 const columns = Math.floor(board.clientWidth / blockSize);
 const rows = Math.floor(board.clientHeight / blockSize);
 
-board.style.gridTemplateColumns = `repeat(${columns}, ${blockSize}px)`;
-board.style.gridTemplateRows = `repeat(${rows}, ${blockSize}px)`;
-
 for (let i = 0; i < rows * columns; i++) {
     const block = document.createElement('div');
     block.classList.add('block');
