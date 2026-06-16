552a269e7d8c514281124b756559fb0dd99d45c2
PRAJWAL-9107 <pjadhav91079107@gmail.com>
2026-06-16 22:31:11 +0530

    frontend structure is ready

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let block = document.getElementById(".bord");
+function fillBlocks(){
+const bord = document.getElementById("bord");
+bord.innerHTML = "";
 
-for (let i = 0; i < 10; i++){
-      const newBlock = document.createElement("div");
-      newBlock.classList.add("newBlock");
-      newBlock.id = "block"
-      block.appendChild(newBlock);
+const bordWidth = bord.clientWidth;
+const bordHeight = bord.clientHeight;
+console.dir(bord)
+const blockSize = 54;
+
+const col = Math.floor(bordWidth / blockSize);
+const row = Math.floor(bordHeight / blockSize);
+
+const totalBlocks = col * row;
+
+for (let i = 1; i <= totalBlocks; i++) {
+      const block = document.createElement("div");
+      block.className = "food";
+      block.textContent = i;
+      bord.appendChild(block);
 }
+}
+
+window.onload = fillBlocks;
+window.onresize = fillBlocks;
